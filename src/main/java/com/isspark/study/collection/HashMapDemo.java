1ab2d8ac551e5bda1cc1b528abc950a3fc8082d8
xuzheng <hx0011001@hengxinyongli.com>
2020-05-08 15:17:10 +0800

    feat:add collection uml

--- src/main/java/com/isspark/study/collection/HashMapDemo.java
+++ src/main/java/com/isspark/study/collection/HashMapDemo.java
@@ -1,11 +1,11 @@
 package com.isspark.study.collection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HashMapDemo {
 
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<String, String>();
+        List<String> list = new ArrayList<>();
     }
 }
