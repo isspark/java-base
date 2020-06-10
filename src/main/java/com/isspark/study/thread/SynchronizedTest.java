305c3cad27b61cb2202164a9a7453a721f27ec9c
isspark <isspark@163.com>
2020-06-10 23:46:46 +0800

    feat:add synchronized test

---
+++ src/main/java/com/isspark/study/thread/SynchronizedTest.java
@@ -0,0 +1,16 @@
+package com.isspark.study.thread;
+
+/**
+ * <p>
+ * TODO(一句话描述该类的功能)
+ * </p>
+ *
+ * @author xuzheng
+ * @since 2020/6/10 22:56
+ */
+public class SynchronizedTest {
+
+    volatile Integer a;
+    public static void main(String[] args) {
+    }
+}
