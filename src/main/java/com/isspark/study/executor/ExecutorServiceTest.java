ba77e12329a232bcd435142088d9237801e95fb8
xuzheng <hx0011001@hengxinyongli.com>
2020-06-24 17:51:27 +0800

    feat:add executor service test

---
+++ src/main/java/com/isspark/study/executor/ExecutorServiceTest.java
@@ -0,0 +1,20 @@
+package com.isspark.study.executor;
+
+import java.util.concurrent.Executors;
+
+/**
+ * @version V1.0
+ * @class: ExecutorServiceTest
+ * @description:
+ * @author: xuzheng
+ * @create: 2020-06-24 17:47
+ **/
+public class ExecutorServiceTest {
+
+    public static void main(String[] args) {
+
+        Executors.newCachedThreadPool();
+    }
+
+
+}
