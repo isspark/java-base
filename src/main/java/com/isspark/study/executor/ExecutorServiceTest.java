a402eb930dae82d3d9d188223a15acbc3211de15
xuzheng <hx0011001@hengxinyongli.com>
2020-06-29 17:34:55 +0800

    feat:add thread pool executor sample

--- src/main/java/com/isspark/study/executor/ExecutorServiceTest.java
+++ src/main/java/com/isspark/study/executor/ExecutorServiceTest.java
@@ -1,6 +1,6 @@
 package com.isspark.study.executor;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @version V1.0
@@ -13,8 +13,25 @@ public class ExecutorServiceTest {
 
     public static void main(String[] args) {
 
-        Executors.newCachedThreadPool();
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        BlockingQueue<Runnable> linkedBlockingDeque = new LinkedBlockingDeque<Runnable>(
+                100);
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5,10, 0L,TimeUnit.MICROSECONDS,linkedBlockingDeque);
+
+        //不建议使用Executors创建线程池
+        //ExecutorService executorService =  Executors.newCachedThreadPool();
+//        pool.submit(new PoolTest());
+        pool.execute(new PoolTest());
+        pool.shutdown();
     }
 
 
+}
+
+class PoolTest implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Hello,World!");
+    }
 }
